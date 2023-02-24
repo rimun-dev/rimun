d364c88b7023065f122627a052f703a550da90e3
Andrea Marino <am.marinoandrea@gmail.com>
2023-02-24 13:07:42 +0100

    [app] Restricted school navbar sections to applicants

--- packages/rimun-app/src/components/singletons/navbars/SchoolNavBar.tsx
+++ packages/rimun-app/src/components/singletons/navbars/SchoolNavBar.tsx
@@ -7,12 +7,15 @@ import {
   UserGroupIcon,
 } from "@heroicons/react/24/outline";
 import React from "react";
+import useAuthenticatedState from "src/utils/useAuthenticatedState";
 import NavBarContainer from "./utils/NavBarContainer";
 import NavBarLink from "./utils/NavBarLink";
 
 interface SchoolNavBarProps {}
 
 const SchoolNavBar: React.FC<SchoolNavBarProps> = () => {
+  const authState = useAuthenticatedState();
+  const hasApplied = authState.account.school!.applications.length > 0;
   return (
     <NavBarContainer>
       <NavBarLink name="News" to="/dashboard/news" icon={NewspaperIcon} />
@@ -25,16 +28,19 @@ const SchoolNavBar: React.FC<SchoolNavBarProps> = () => {
         name="Students"
         to="/dashboard/students"
         icon={UserGroupIcon}
+        active={hasApplied}
       />
       <NavBarLink
         name="Teachers"
         to="/dashboard/teachers"
         icon={AcademicCapIcon}
+        active={hasApplied}
       />
       <NavBarLink
         name="Delegates"
         to="/dashboard/delegations"
         icon={FlagIcon}
+        active={hasApplied}
       />
       <NavBarLink name="Settings" to="/dashboard/settings" icon={CogIcon} />
     </NavBarContainer>
