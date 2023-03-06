01113b6ab11748871e189eca7fd423d416ced66c
Andrea Marino <am.marinoandrea@gmail.com>
2023-03-06 14:43:17 +0100

    [api] Added migration for forum images and updated seed script

---
+++ packages/rimun-api/prisma/migrations/20230228130924_/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "forum" ADD COLUMN     "image_path" TEXT;
--- packages/rimun-api/prisma/seed.ts
+++ packages/rimun-api/prisma/seed.ts
@@ -9,13 +9,94 @@ const prisma = new PrismaClient();
 async function main() {
   await prisma.forum.createMany({
     data: [
-      { acronym: "GA", name: "General Assembly" },
-      { acronym: "ECOSOC", name: "Economic and Social Council" },
-      { acronym: "HRC", name: "Human Rights Council" },
-      { acronym: "SC", name: "Security Council" },
-      { acronym: "ICJ", name: "International Court of Justice" },
-      { acronym: "CC", name: "Crisis Committee" },
-      { acronym: "HSC", name: "Historical Security Council" },
+      {
+        acronym: "GA",
+        name: "General Assembly",
+        image_path: "img/forums/ga.jpg",
+        description: `
+The **General Assembly** (GA) occupies a central position as the chief deliberative, policymaking and representative organ of the United Nations. The General Assembly comprises all 193 Member States of the United Nations. Each Member state has one vote and shall have not more than five representatives in the General Assembly.
+
+The General Assembly is composed by six Committees that meet separately in the first three days of the conference; each Committee drafts and debates resolutions concerning its mandate, that includes vast topics related to peace, security, disarmament, development, environment and human rights. The General Assembly will discuss passed resolutions during the plenary session, which occurs on the last day of the conference.
+      `,
+      },
+      {
+        acronym: "ECOSOC",
+        name: "Economic and Social Council",
+        image_path: "img/forums/ecosoc.jpg",
+        description: `
+The **Economic and Social Council** (ECOSOC) is one of the six main organs of the United Nations. It is the principal body for coordination, policy review, policy dialogue and recommendations on economic, social and environmental issues, as well as for implementation of the internationally agreed development goals. ECOSOC is made of six Commissions which take care of different duties.
+
+In the first phase of the Conference all six Commissions work separately and independently discussing three topics each and writing down resolutions. In the second and last part, all chairs are called to choose a resolution per commission which is going to be discussed in the Plenary Session, where Commissions are gathered together, held by the President of ECOSOC.
+      `,
+      },
+      {
+        acronym: "HRC",
+        name: "Human Rights Council",
+        image_path: "img/forums/hrc.jpg",
+        description: `
+The **Human Rights Council** (HRC) is the main inter-governmental body within the United Nations system whose attention is focused on the protection of human rights and the resolution of human rights violations.
+
+The range of topics discussed is very wide and can concern all thematic human rights issues and situations that require the attention of the council throughout the year.
+
+The Human Rights Council is composed of 47 member states elected by the members of the General Assembly. The Advisory Committee functions as a think-tank for the Council, it is a specialized committee composed of 18 independent experts who mainly focus on studies and research-based advice. During the first three days of Conference the two bodies of HRC and AC work independently one from each other writing, debating and voting resolutions. The President of the HRC, helped by the vice presidents and chairs, coordinates the discussion on the last day of the conference in which both bodies meet together in order to share the resolutions passed.      
+`,
+      },
+      {
+        acronym: "SC",
+        name: "Security Council",
+        image_path: "img/forums/sc.jpg",
+        description: `
+The United Nations **Security Council** (SC) is one of the principal organs of the United Nations and is charged with the maintenance of international peace and security. Its powers include the establishment of peacekeeping operations, the establishment of international sanctions, and the authorization of military action. Under the Charter of the United Nations, all Member States are obligated to comply with Council decisions.
+
+SC is composed of 15 members. In these five veto-wielding permanent members (P5)—China, France, Russia, the United Kingdom, and the United State—are included. In addition to the P5, there are 10 non-permanent members, with five elected each year to serve two-year terms.
+
+The SC deals with very hot issues on the agenda that primarily concern international peace, stability and security and this is the reason why every solution is deeply discussed in order to be adopted. The rules of procedure are settled by the President and the Vice-President who are also in charge of guarantee that veto is not overused.      
+`,
+      },
+      {
+        acronym: "ICJ",
+        name: "International Court of Justice",
+        image_path: "img/forums/icj.jpg",
+        description: `
+The **International Court of Justice** (ICJ) is the judicial organ of the United Nations. 193 States are currently Members of the United Nations and recognize as compulsory the jurisdiction of the Court. The Court’s role is to settle, in accordance with international law, legal disputes submitted to it by States and to give advisory opinions on legal questions referred to it by authorized United Nations organs and specialized agencies.
+
+The court is composed by the Presidency (Head of the ICJ, President of the ICJ and Vice-President), the Registry, four advocates (two per party) and an odd number of judges varying between 15 and 19.
+
+A judge must be a person of high moral character, who possess the qualifications required in their respective countries for appointment to the highest judicial offices, or are jurisconsults of recognized competence in international law. Once elected, a Member of the Court is a delegate neither of the government of his own country nor of that of any other State. Unlike most other organs of international organizations, the Court is not composed of representatives of governments. Members of the Court are independent judges whose first task, before taking up their duties, is to make a solemn declaration in open court that they will exercise their powers impartially and conscientiously.
+`,
+      },
+      {
+        acronym: "CC",
+        name: "Crisis Committee",
+        image_path: "img/forums/cc.jpg",
+        description: `
+The **Crisis Committee** (CC) was established by The Secretariat of the XI session of RIMUN, and as the name implies, it deals with worldwide crises.
+
+The delegates will be presented with a crisis: their mission? Avoid catastrophe, save lives, and possibly solve the crisis for good. The crises will be designed by our team of RIMUN Crisis writers, who will try to create the most realistic, engaging, and challenging fictional crises for delegates to solve. The crises will naturally be based on real life events, but will be infused with twists and developments, that will depend heavily on the delegate’s actions, and interactions with each other inside the Committee.
+
+The main feature of the Crisis Committee is that it works in real time, therefore no information about the crises will be provided beforehand. At the completion of each crisis, information on the new one will be provided. As such it will not be possible to prepare amendments in advance.
+
+The delegates who will participate in the Crisis Committee will be required to submit a position paper on their assigned country and they will be required to study their assigned country’s stats (Population, religion, GDP, Interests etc), foreign policy, and a general understanding of the domestic policy. This is necessary to ensure that the delegates are able to grasp the implications of certain events in the crisis, as well as make informed decisions as to what to do.            
+      `,
+      },
+      {
+        acronym: "HSC",
+        name: "Historical Security Council",
+        image_path: "img/forums/hsc.jpg",
+        description: `
+The **Historical Security Council** (HSC) is a forum reserved to undergraduate students, which sets the well-known UN Security Council back to one of the most significant chapters of the history of the contemporary world.
+
+With the power held by the Council, delegates are called upon to make important decisions concerning political crises that could reshape the course of history as it's known. Furthermore, the Security Council’s composition and participating Nations are based on the chosen historical period and any event following the starting year can't be mentioned as it hadn’t happened in the simulated setting yet.
+
+The Security Council must take its decisions in the interest of international peace and maintain a secure environment for the citizens of the world: its powers include the establishment of peacekeeping operations, the formulation of international sanctions, and the authorization of military action.
+
+The Security Council is composed of fifteen nnmembers; of these, five veto-helding permanent members are present since the constitution of the United Nations Organisation: these are commonly known as the P5, and comprise China, France, Russia, the United Kingdom, and the United States of America; the remaining ten member States of the Security Council are non-permanent.
+
+In the case of the Historical Security Council, present member States will reprise the geopolitical structure pertinent to the period in which the forum is set for the duration of the conference (e.g. Russia will be the USSR if the setting takes place between 1945 and 1991).
+
+What makes this forum unique is that Historical Security Council’s delegates will immerse themselves in times of engaging geopolitical issues, trying to solve them to the best of their abilities: it is a extraordinary and remarkable experience that will enrich your understanding of the passed that shaped today’s world.      
+`,
+      },
     ],
   });
 
@@ -57,11 +138,19 @@ async function main() {
     data: [
       { group_id: 1, name: "Secretary General" },
       { group_id: 1, name: "Conference Director" },
-      { group_id: 1, name: "President of the General Assembly" },
-      { group_id: 1, name: "President of the Eco.So.C." },
-      { group_id: 1, name: "President of the Human Rights Council" },
-      { group_id: 1, name: "President of the Security Council" },
-      { group_id: 1, name: "President of the Historical Security Council" },
+      { group_id: 1, name: "President of the General Assembly", forum_id: 1 },
+      { group_id: 1, name: "President of the Eco.So.C.", forum_id: 2 },
+      {
+        group_id: 1,
+        name: "President of the Human Rights Council",
+        forum_id: 3,
+      },
+      { group_id: 1, name: "President of the Security Council", forum_id: 4 },
+      {
+        group_id: 1,
+        name: "President of the Historical Security Council",
+        forum_id: 7,
+      },
       {
         group_id: 1,
         name: "Conference Manager for Relations with International Delegations",
@@ -77,18 +166,27 @@ async function main() {
       },
       { group_id: 1, name: "Housing Manager" },
       { group_id: 1, name: "Transfer Manager" },
-      { group_id: 1, name: "Head of ICJ" },
-      { group_id: 4, name: "Vice President of the Human Rights Council" },
-      { group_id: 4, name: "Vice President of the Security Council" },
+      { group_id: 1, name: "Head of ICJ", forum_id: 5 },
+      {
+        group_id: 4,
+        name: "Vice President of the Human Rights Council",
+        forum_id: 3,
+      },
+      {
+        group_id: 4,
+        name: "Vice President of the Security Council",
+        forum_id: 4,
+      },
       {
         group_id: 4,
         name: "Vice President of the Historical Security Council",
+        forum_id: 7,
       },
       { group_id: 4, name: "Main Chair" },
       { group_id: 4, name: "Deputy Chair" },
       { group_id: 5, name: "Delegate" },
-      { group_id: 6, name: "President of the ICJ" },
-      { group_id: 6, name: "Vice President of the ICJ" },
+      { group_id: 6, name: "President of the ICJ", forum_id: 5 },
+      { group_id: 6, name: "Vice President of the ICJ", forum_id: 5 },
       { group_id: 6, name: "Register" },
       { group_id: 6, name: "Judge" },
       { group_id: 6, name: "Advocate" },
